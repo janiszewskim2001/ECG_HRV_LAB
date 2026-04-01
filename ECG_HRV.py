6e9e955ad5c01d1baa5f544c57e4580bab601d5f
janiszewskim2001 <janiszewskim2001@gmail.com>
2026-04-01 15:59:54 +0200

    Modify ECG signal and update visualization
    Added a sine wave component to the ECG signal and updated the plot to reflect the modified signal.

--- ECG_HRV.py
+++ ECG_HRV.py
@@ -77,6 +77,7 @@ min_czas, max_czas = float(df_all['czas'].min()), float(df_all['czas'].max())
 zakres_czasu = st.slider("Zakres czasu do analizy [s]:", min_czas, max_czas, (min_czas, min(min_czas + 20.0, max_czas)), 0.1)
 
 df = df_all[(df_all['czas'] >= zakres_czasu[0]) & (df_all['czas'] <= zakres_czasu[1])].copy()
+df['ecg'] = df['ecg'] + 1.5 * np.sin(2 * np.pi * 0.5 * df['czas'])#psujemy
 
 if len(df) > 28000:
     st.warning("Uwaga: W sekcji EMD zostanie przetworzone tylko pierwsze 28000 próbek ze względu na obciążenie procesora.")
@@ -93,7 +94,7 @@ with colB:
 with st.container(border=True):
     fig_glowny = go.Figure()
     fig_glowny.add_trace(go.Scatter(x=df_all['czas'], y=df_all['ecg'], name='Pozostała część', line=dict(color="#444444", width=1)))
-    fig_glowny.add_trace(go.Scatter(x=df_all['czas'], y=df_all['ecg']-0.3*np.sin(5 * df_all['czas']), name='Zepsuty fragment EKG', line=dict(color=kolor_sygnalu, width=2)))
+
     fig_glowny.add_trace(go.Scatter(x=df['czas'], y=df['ecg'], name='Fragment do analizy', line=dict(color=kolor_sygnalu, width=2)))
     fig_glowny = aplikuj_ciemny_motyw(fig_glowny)
     fig_glowny.update_layout(xaxis_title="Czas [s]", yaxis_title="Amplituda [mV]")
